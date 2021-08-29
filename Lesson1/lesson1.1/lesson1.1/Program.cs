6e074cdce763ee141c0b03fbc7391db15b8708ed
Renata <vagapova.rena@list.ru>
2021-08-29 23:54:39 +0300

    HelloWorld

---
+++ Lesson1/lesson1.1/lesson1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine("Hello, " + name + ", today is " + date.ToShortDateString());
+            Console.ReadLine();
+        }
+    }
+}
