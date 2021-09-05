6df99c78ec6982a28d0deb309a4b69984d7bbd41
Renata <vagapova.rena@list.ru>
2021-09-06 00:57:16 +0300

    1.Mass in diagonal 2.Name and number 3.BackWards

---
+++ Lesson3/lesson3.3/lesson3.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lesson3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            char[] mass = str.ToCharArray();
+            Array.Reverse(mass);
+            Console.Write("Вывод вашей строки в обратном порядке: ");
+            Console.Write(mass);
+        }
+    }
+}
