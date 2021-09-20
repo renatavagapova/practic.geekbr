9113c7fc84795d60956aecd8923a9efc4a06d4db
Renata <vagapova.rena@list.ru>
2021-09-20 20:08:28 +0300

    Lesson 5
    1. Random in text file
    2. Time in text file
    3. Byte in binary code
    4.  - 
    5.  -

---
+++ Lesson5/lesson5.3/lesson5.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace lesson5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "test.bin";
+            Console.WriteLine("Введите произвольный набор чисел от 0 до 255");
+            var input = Console.ReadLine().Split(' ');
+            var array = new byte[input.Length];
+            for (int i =0; i<array.Length; i++)
+            {
+                if (!byte.TryParse(input[i],out byte number)) 
+                {
+                    Console.WriteLine("Вы ввели неверные значения");
+                    return;
+                }
+                array[i] = number;
+            }
+            // запись данных в двоичном формате
+            using (BinaryWriter writer = new BinaryWriter(File.OpenWrite(filename)))
+            {
+                writer.Write(array);
+            }
+            Console.WriteLine($"Что сейчас находится в файле {filename} :");
+            // считывается запись данных в двоичном формате
+            byte[] newArray;
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(filename)))
+            {
+                newArray = reader.ReadBytes(array.Length);
+            }
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.Write(newArray[i] + " ");
+            }
+
+        }
+            
+    }
+}
---
+++ Lesson5/lesson5.5/lesson5.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
